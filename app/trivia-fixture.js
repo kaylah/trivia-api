a27127a002fbea62904dba6cdf7d340457905fe9
Kayla Hennegin <kayla@bellafaithdesign.com>
2016-10-01 00:02:54 -0500

    initial trivia api commit

---
+++ app/trivia-fixture.js
@@ -0,0 +1,212 @@
+module.exports = [
+  {
+    "question": "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "Ted"
+      },
+      {
+        "status": "correct",
+        "answer": "Harry Potter"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Spy Kids"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Pirates of the Caribbean"
+      }
+    ]
+  },
+  {
+    "question": "When was the movie &#039;Con Air&#039; released?",
+    "answers": [
+      {
+        "status": "correct",
+        "answer": "1997"
+      },
+      {
+        "status": "incorrect",
+        "answer": "1985"
+      },
+      {
+        "status": "incorrect",
+        "answer": "1999"
+      },
+      {
+        "status": "incorrect",
+        "answer": "1990"
+      }
+    ]
+  },
+  {
+    "question": "Who played Deputy Marshal Samuel Gerard in the 1993 film &quot;The Fugitive&quot;?",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "Harrison Ford"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Harvey Keitel"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Martin Landau"
+      },
+      {
+        "status": "correct",
+        "answer": "Tommy Lee Jones"
+      }
+    ]
+  },
+  {
+    "question": "Who starred in the film 1973 movie &quot;Enter The Dragon&quot;?",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "Yun-Fat Chow"
+      },
+      {
+        "status": "correct",
+        "answer": "Bruce Lee"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Jackie Chan"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Jet Li"
+      }
+    ]
+  },
+  {
+    "question": "What was the title of the first Bond movie, released in 1962?",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "Goldfinger"
+      },
+      {
+        "status": "incorrect",
+        "answer": "From Russia with Love"
+      },
+      {
+        "status": "correct",
+        "answer": "Dr. No"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Thunderball"
+      }
+    ]
+  },
+  {
+    "question": "This movie contains the quote, &quot;Houston, we have a problem.&quot;",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "Marooned"
+      },
+      {
+        "status": "incorrect",
+        "answer": "The Right Stuff"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Capricorn One"
+      },
+      {
+        "status": "correct",
+        "answer": "Apollo 13"
+      }
+    ]
+  },
+  {
+    "question": "This movie contains the quote, &quot;I feel the need ... the need for speed!&quot;",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "The Color of Money"
+      },
+      {
+        "status": "correct",
+        "answer": "Top Gun"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Days of Thunder"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Cocktail"
+      }
+    ]
+  },
+  {
+    "question": "This movie contains the quote, &quot;Nobody puts Baby in a corner.&quot;",
+    "answers": [
+      {
+        "status": "correct",
+        "answer": "Dirty Dancing"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Three Men and a Baby"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Ferris Bueller&#039;s Day Off"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Pretty in Pink"
+      }
+    ]
+  },
+  {
+    "question": "What is the orange and white bot&#039;s name in &quot;Star Wars: The Force Awakens&quot;?",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "BB-3"
+      },
+      {
+        "status": "incorrect",
+        "answer": "AA-A"
+      },
+      {
+        "status": "correct",
+        "answer": "BB-8"
+      },
+      {
+        "status": "incorrect",
+        "answer": "R2-D2"
+      }
+    ]
+  },
+  {
+    "question": "Which of these movies did Jeff Bridges not star in?",
+    "answers": [
+      {
+        "status": "incorrect",
+        "answer": "True Grit"
+      },
+      {
+        "status": "incorrect",
+        "answer": "The Giver"
+      },
+      {
+        "status": "incorrect",
+        "answer": "Tron: Legacy"
+      },
+      {
+        "status": "correct",
+        "answer": "The Hateful Eight"
+      }
+    ]
+  }
+]
---
+++ server.js
@@ -0,0 +1,32 @@
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+var sampleTrivia = require('./app/trivia-fixture.js');
+
+//var mongoose   = require('mongoose');
+//mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router(); 
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.route('/sample')
+
+  .get(function(req, res) {
+      
+    res.json(sampleTrivia);  
+      
+  });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
